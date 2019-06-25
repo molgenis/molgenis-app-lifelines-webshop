15c1dfafe74189962c36f451a5697250446099ea
Fleur Kelpin <f.kelpin@umcg.nl>
2019-06-26 00:06:30 +0200

    feat: use filters

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -2,7 +2,6 @@ import ApplicationState from '@/types/ApplicationState'
 // @ts-ignore
 import { transformToRSQL } from '@molgenis/rsql'
 import Getters from '@/types/Getters'
-import Count from '@/types/Count'
 
 export default {
   variantIds: (state: ApplicationState) =>
@@ -16,17 +15,78 @@ export default {
       )
     ),
   rsql: (state: ApplicationState, getters: Getters) => {
-    const operands = []
+    let operands: Object[] = []
     if (getters.variantIds.length > 0) {
-      const variantFilter = {
+      operands.push({
         selector: 'variant_id',
         comparison: '=in=',
         arguments: getters.variantIds
-      }
-      operands.push(variantFilter)
+      })
+    } else return ''
+    if (state.facetFilter.ageGroupAt1A.length > 0) {
+      operands.push({
+        operator: 'OR',
+        operands: state.facetFilter.ageGroupAt1A.map((ageGroup) => ({
+          selector: 'll_nr.age_group_at_1a',
+          comparison: '==',
+          arguments: ageGroup
+        }))
+      })
     }
-    if (operands.length === 0) {
-      return ''
+    if (state.facetFilter.ageGroupAt2A.length > 0) {
+      operands.push({
+        operator: 'OR',
+        operands: state.facetFilter.ageGroupAt2A.map((ageGroup) => ({
+          selector: 'll_nr.age_group_at_2a',
+          comparison: '==',
+          arguments: ageGroup
+        }))
+      })
+    }
+    if (state.facetFilter.ageGroupAt3A.length > 0) {
+      operands.push({
+        operator: 'OR',
+        operands: state.facetFilter.ageGroupAt3A.map((ageGroup) => ({
+          selector: 'll_nr.age_group_at_3a',
+          comparison: '==',
+          arguments: ageGroup
+        }))
+      })
+    }
+    if (state.facetFilter.subcohort.length > 0) {
+      operands = [
+        ...operands,
+        ...state.facetFilter.subcohort.map(subcohort => ({
+          selector: `ll_nr.subcohort${subcohort}_group`,
+          comparison: '==',
+          arguments: true
+        })
+        )]
+    }
+    if (state.facetFilter.gender.length > 0) {
+      operands.push({
+        operands: state.facetFilter.gender.map(
+          (gender) => ({
+            selector: 'll_nr.gender_group',
+            comparison: '==',
+            arguments: gender
+          })),
+        operator: 'OR'
+      })
+    }
+    if (state.facetFilter.yearOfBirthRange.length > 0) {
+      operands.push({
+        selector: 'll_nr.year_of_birth',
+        comparison: '=ge=',
+        arguments: state.facetFilter.yearOfBirthRange[0]
+      })
+    }
+    if (state.facetFilter.yearOfBirthRange.length > 1) {
+      operands.push({
+        selector: 'll_nr.year_of_birth',
+        comparison: '=le=',
+        arguments: state.facetFilter.yearOfBirthRange[1]
+      })
     }
     return transformToRSQL({
       operator: 'AND',
