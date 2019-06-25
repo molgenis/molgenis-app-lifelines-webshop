b5e01ec47339000a06ce3b09caa2e90d7d6dd900
sidohaakma <sido@haakma.org>
2019-06-25 11:35:23 +0200

    fix(test): resolve merge conflicts with master

--- src/types/applicationState.ts
+++ src/types/applicationState.ts
@@ -6,11 +6,14 @@ export default interface ApplicationState {
   ageGroupOptions: FacetOption[],
   ageAtOptions: FacetOption[],
   facetFilter: {
-    gender: Strin    ageAtOptions: FacetOption[],    facetFilter: {      gender: String[],        subcohort: String[],      ageGroupAt1A: String[,],s  aeGroupAt2A: String[],	      ageGroupAt3A: String]t,
-      yearOfBirthRange: Number[]
-    }
-  }	  sectionLis
-t: String ,
+    gender: String[],
+    subcohort: String[],
+    ageGroupAt1A: String[],
+    ageGroupAt2A: String[],	      
+    ageGroupAt3A: String[],
+    yearOfBirthRange: Number[]
+  },	  
+  sectionList: String[],
   subSectionList: String[],
   treeStructure: Object[]
   treeSelected: String | null
