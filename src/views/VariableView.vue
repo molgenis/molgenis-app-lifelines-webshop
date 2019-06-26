496214432308f2588d47859b94761b4aa78a2635
connor <c.stroomberg@gmail.com>
2019-06-26 12:37:34 +0200

    wip add tabs

--- src/views/VariableView.vue
+++ src/views/VariableView.vue
@@ -1,7 +1,20 @@
 <template>
   <div id="variable-view" class="container row">
     <div class="col-12" >
-        <h2> Varaible view </h2>
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Variables</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Selection</a>
+            </li>
+        </ul>
+        <div>
+            Variables
+        </div>
+        <div>
+            Selection
+        </div>
     </div>
   </div>
 </template>
