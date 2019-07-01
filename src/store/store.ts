e7007ae44ca70b2edd15133ecf3f1c48034581c0
connoratrug <47183404+connoratrug@users.noreply.github.com>
2019-07-01 13:45:34 +0200

    Update store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import state from '@/store/state'
-// @ts-ignore
-import api from '@molgenis/molgenis-api-client'
 
 import actions from '@/store/actions'
 import getters from '@/store/getters'
