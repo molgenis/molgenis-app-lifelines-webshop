8c5eb93811aa783878d7e295f2d1ccfd7fdcdf2b
Fleur Kelpin <f.kelpin@umcg.nl>
2019-07-02 16:07:39 +0200

    style: revert changes to store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import state from '@/store/state'
-
 import actions from '@/store/actions'
 import getters from '@/store/getters'
 import mutations from '@/store/mutations'
