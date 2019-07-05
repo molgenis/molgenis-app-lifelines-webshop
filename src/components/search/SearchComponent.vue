baa41e8552cb10d523f08dbd59698e9c6b270135
connor <c.stroomberg@gmail.com>
2019-07-05 09:41:41 +0200

    feat(search) wip initial commit

---
+++ src/components/search/SearchComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <input class="form-control" type="search" placeholder="Search..." :value="value" @change="handleSeachValueChange"/>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'SearchComponent',
+  props: {
+    value: {
+      type: String
+    }
+  },
+  methods: {
+    handleSeachValueChange () {
+      $emit('seachChanged', this.value)
+    }
+  }
+})
+</script>
--- src/views/ContentView.vue
+++ src/views/ContentView.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="Content-view">
-    <h3>2. Select data</h3>
-        <div class="row mt-3" >
-          <div class="col-4" >
-            <tree-view  />
+      <div class="row" >
+          <div class="col-3" >
+            <h3>2. Select data</h3>
           </div>
-          <div class="col-8" >
-            <grid-view />
+          <div class="col-span-4 col-4" >
+            <search-component></search-component>
           </div>
+      </div>
+      <div class="row mt-3" >
+        <div class="col-4" >
+          <tree-view  />
         </div>
+        <div class="col-8" >
+          <grid-view />
+        </div>
+      </div>
   </div>
 </template>
 
@@ -16,10 +23,11 @@
 import Vue from 'vue'
 import TreeView from './TreeView.vue'
 import GridView from './GridView.vue'
+import SearchComponent from '../components/search/SearchComponent.vue'
 
 export default Vue.extend({
   name: 'ContentView',
-  components: { TreeView, GridView }
+  components: { TreeView, GridView, SearchComponent }
 })
 
 </script>
