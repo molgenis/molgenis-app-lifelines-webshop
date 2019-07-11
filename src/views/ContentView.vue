dc233ee6b04e230d9251301750b6b322c6e4ee08
connoratrug <47183404+connoratrug@users.noreply.github.com>
2019-07-11 16:21:39 +0200

    Feat/search variables (#66)
    feat(search): Search input component

--- src/views/ContentView.vue
+++ src/views/ContentView.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="Content-view">
-    <div class="row flex-nowrap">
+      <div class="row flex-nowrap" >
       <div class="col tree">
-        <h3 class="my-3">2. Select data</h3>
-        <tree-view  />
+           <h3 class="my-3">2. Select data</h3>
+          </div>
+          <div class="col-span-4 col-4" >
+            <search-component v-model="searchValue"></search-component>
+          </div>
       </div>
-      <div class="col">
-        <grid-view />
+      <div class="row mt-3" >
+        <div class="col-4" >
+          <tree-view  />
+        </div>
+        <div class="col-8" >
+          <grid-view />
+        </div>
       </div>
-    </div>
+
   </div>
 </template>
 
@@ -16,10 +24,16 @@
 import Vue from 'vue'
 import TreeView from './TreeView.vue'
 import GridView from './GridView.vue'
+import SearchComponent from '../components/search/SearchComponent.vue'
 
 export default Vue.extend({
   name: 'ContentView',
-  components: { TreeView, GridView }
+  components: { TreeView, GridView, SearchComponent },
+  data: () => {
+    return {
+      searchValue: ''
+    }
+  }
 })
 </script>
 
