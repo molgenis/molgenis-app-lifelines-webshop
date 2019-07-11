2c14eb9beece2edde180115d2c97d673411627bb
connor <c.stroomberg@gmail.com>
2019-07-11 16:14:40 +0200

    task(search): lint code

--- tests/unit/components/search/SearchComponent.spec.ts
+++ tests/unit/components/search/SearchComponent.spec.ts
@@ -3,45 +3,42 @@ import Vue from 'vue'
 import { shallowMount, Wrapper } from '@vue/test-utils'
 
 describe('Search Component', () => {
-    let wrapper: Wrapper<Vue>
+  let wrapper: Wrapper<Vue>
 
-    describe('when no props are passed', () => {
-        beforeEach(() => {
-            wrapper = shallowMount(SearchComponent)
-          })
-          
-          it('should render a search input', () => {
-              expect(wrapper.find('input')).toBeTruthy()
-          })
+  describe('when no props are passed', () => {
+    beforeEach(() => {
+      wrapper = shallowMount(SearchComponent)
     })
 
-    describe('when search prop is passed', () => {
-        beforeEach(() => {
-            wrapper = shallowMount(SearchComponent, {propsData: {
-                value: 'testing'
-            }})
-          })
-          
-        it('should render the passed search prop as search value', () => {
-            // @ts-ignore
-            expect(wrapper.find('input').element.value).toEqual('testing')
-        })
+    it('should render a search input', () => {
+      expect(wrapper.find('input')).toBeTruthy()
     })
-   
-    describe('when a search value is entered', () => {
-        beforeEach(() => {
-            wrapper = shallowMount(SearchComponent)
-            wrapper.find('input').setValue('dem')
-          })
-          
-        it('should emit a search val changed event', (done) => {
-            setTimeout(() => {
-                expect(wrapper.emitted().seachChanged[0][0]).toEqual('dem')
-                done()
-            }, 300)
-           
-        })
+  })
+
+  describe('when search prop is passed', () => {
+    beforeEach(() => {
+      wrapper = shallowMount(SearchComponent, { propsData: {
+        value: 'testing'
+      } })
     })
 
-  
+    it('should render the passed search prop as search value', () => {
+      // @ts-ignore
+      expect(wrapper.find('input').element.value).toEqual('testing')
+    })
+  })
+
+  describe('when a search value is entered', () => {
+    beforeEach(() => {
+      wrapper = shallowMount(SearchComponent)
+      wrapper.find('input').setValue('dem')
+    })
+
+    it('should emit a search val changed event', (done) => {
+      setTimeout(() => {
+        expect(wrapper.emitted().seachChanged[0][0]).toEqual('dem')
+        done()
+      }, 300)
+    })
+  })
 })
