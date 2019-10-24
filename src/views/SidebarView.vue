e36a548b2730170a14499f23021ac84e1f45822b
MaxPostema <max@elynor.com>
2019-10-24 11:52:12 +0200

    fix: IE11 fixes for tree view and select cohorts

--- src/views/SidebarView.vue
+++ src/views/SidebarView.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="sidebar-view" :class="{'hide-bar':!value}">
-    <div class="label" @click="toggleVisibility" v-click-outside="hide">{{ 'lifelines-webshop-sidebar-header' | i18n }}</div>
+  <div id="sidebar-view" v-click-outside="hide" :class="{'hide-bar':!value}">
+    <div class="label" @click="toggleVisibility">{{ 'lifelines-webshop-sidebar-header' | i18n }}<font-awesome-icon icon="angle-double-down" class="ml-2" /></div>
     <div class="overflow-hidden">
       <div class="sidebar-width">
         <h3 class="px-4">{{ 'lifelines-webshop-sidebar-header' | i18n }}</h3>
         <ul class="list-unstyled sidebar-content p-4">
-          <li class="hide-sidebar" @click="hide"><font-awesome-icon icon="minus-circle" size="lg" /></li>
+          <li class="hide-sidebar" @click="hide"><font-awesome-icon icon="angle-double-left" size="lg" /></li>
           <li>
             <facet-container
               facetId="age"
@@ -64,11 +64,11 @@ import AgeFacet from '../components/facets/AgeFacet.vue'
 import RangeFacet from '../components/facets/RangeFacet.vue'
 import { mapMutations } from 'vuex'
 import CountView from '@/views/CountView'
-import ClickOutside from 'vue-click-outside'
+import ClickOutside from 'v-click-outside'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faMinusCircle } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDoubleLeft, faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(faMinusCircle)
+library.add(faAngleDoubleLeft, faAngleDoubleDown)
 
 export default Vue.extend({
   name: 'SidebarView',
@@ -88,6 +88,7 @@ export default Vue.extend({
   },
   methods: {
     hide () {
+      console.log('click')
       this.$emit('input', false)
     },
     toggleVisibility () {
@@ -173,7 +174,7 @@ export default Vue.extend({
   mounted () {
     this.popupItem = this.$el
   },
-  directives: { ClickOutside }
+  directives: { clickOutside: ClickOutside.directive }
 })
 </script>
 
