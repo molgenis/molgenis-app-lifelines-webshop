6203b720a499ddafbce3b010d0e982ce8fc99094
connor <c.stroomberg@gmail.com>
2019-11-07 20:02:29 +0100

    update snapshot

--- tests/unit/fixtures/orders.ts
+++ tests/unit/fixtures/orders.ts
@@ -3,18 +3,18 @@ import { Order, OrderState } from '@/types/Order'
 const orders: Order[] = [{
   orderNumber: 'edcba',
   name: null,
-  creationDate: '2019-10-31T13:48:12Z',
-  submissionDate: '2019-10-30T13:25:49Z',
-  updateDate: '2019-10-31T13:48:12Z',
+  creationDate: null,
+  submissionDate: null,
+  updateDate: null,
   projectNumber: null,
   applicationForm: null,
   state: OrderState.Draft
 }, {
   orderNumber: 'abcde',
   name: 'My draft order',
-  creationDate: '2019-10-30T13:25:49Z',
-  submissionDate: '2019-10-30T13:25:49Z',
-  updateDate: '2019-10-30T13:25:49Z',
+  creationDate: null,
+  submissionDate: null,
+  updateDate: null,
   projectNumber: null,
   applicationForm: {
     filename: 'Motivation.pdf',
