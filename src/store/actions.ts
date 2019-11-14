6d3f0c0e180e2781e67452f52d69de1c862f3bba
connor <c.stroomberg@gmail.com>
2019-11-14 14:55:48 +0100

    refactor actions to use molgenis-js-client instead of api client

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import api from '@molgenis/molgenis-api-client'
+import api from '@molgenis/molgenis-js-client'
 import { tryAction, toCart, fromCart } from './helpers'
 import { Variable } from '@/types/Variable'
 import Assessment from '@/types/Assessment'
@@ -14,7 +14,11 @@ import { OrderState } from '@/types/Order'
 import moment from 'moment'
 import { TreeParent } from '@/types/Tree'
 
-const buildPostOptions = (formData: any, formFields: FormField[]) => {
+interface PostOptions extends RequestInit {
+  body: FormData
+}
+
+const buildPostOptions = (formData: any, formFields: FormField[]):PostOptions => {
   return {
     headers: {
       'Accept': 'application/json',
@@ -35,10 +39,10 @@ const createOrder = async (formData: any, formFields:FormField[]) => {
 
   let reTryCount = 0
 
-  const trySubmission = () => {
+  const trySubmission = ():any => {
     const orderNumber = generateOrderNumber().toString()
     options.body.set('orderNumber', orderNumber)
-    return api.post('/api/v1/lifelines_order', options, true).then(() => {
+    return api.post('/api/v1/lifelines_order', options).then(() => {
       return orderNumber
     }, (error:any) => {
       // OrderNumber must be unique, just guess untill we find one
@@ -57,7 +61,7 @@ const createOrder = async (formData: any, formFields:FormField[]) => {
 const updateOrder = async (formData: any, formFields:FormField[]) => {
   const options = buildPostOptions(formData, formFields)
 
-  return api.post(`/api/v1/lifelines_order/${formData.orderNumber}?_method=PUT`, options, true).then(() => {
+  return api.post(`/api/v1/lifelines_order/${formData.orderNumber}?_method=PUT`, options).then(() => {
     return formData.orderNumber
   })
 }
@@ -65,7 +69,7 @@ const updateOrder = async (formData: any, formFields:FormField[]) => {
 export default {
   loadOrders: tryAction(async ({ commit }: any) => {
     commit('setOrders', null)
-    const response = await api.get('/api/v2/lifelines_order?num=10000')
+    const response = await api.get<any>('/api/v2/lifelines_order?num=10000')
     commit('setOrders', response.items)
   }),
   deleteOrder: tryAction(async ({ dispatch, commit }: any, orderId: string) => {
@@ -75,9 +79,10 @@ export default {
   }),
   loadSections: tryAction(async ({ commit, state } : any) => {
     if (!Object.keys(state.sections).length) {
-      const response = await api.get('/api/v2/lifelines_section?num=10000')
+      const response = await api.get<any>('/api/v2/lifelines_section?num=10000')
+      const jsonData = await response.json()
       commit('updateSections'
-        , response.items.reduce((sections: { [key:number]: Section }, item:any) => {
+        , jsonData.items.reduce((sections: { [key:number]: Section }, item:any) => {
           sections[item.id] = item
           return sections
         }, {}))
@@ -85,17 +90,19 @@ export default {
   }),
   loadSubSections: tryAction(async ({ commit, state } : any) => {
     if (state.subSectionList.length === 0) {
-      const response = await api.get('/api/v2/lifelines_sub_section?num=10000')
+      const response = await api.get<any>('/api/v2/lifelines_sub_section?num=10000')
+      const jsonData = await response.json()
       let subSections:string[] = []
-      response.items.map((item:any) => { subSections[item.id] = item.name })
+      jsonData.items.map((item:any) => { subSections[item.id] = item.name })
       commit('updateSubSections', subSections)
     }
   }),
   loadSectionTree: tryAction(async ({ commit, state } : any) => {
     if (state.treeStructure.length === 0) {
-      const response = await api.get('/api/v2/lifelines_tree?num=10000')
+      const response = await api.get<any>('/api/v2/lifelines_tree?num=10000')
+      const jsonData = await response.json()
       let structure: {[id: number]: number[]} = {}
-      response.items.map((item: any) => {
+      jsonData.items.map((item: any) => {
         if (item.section_id.id in structure) {
           structure[item.section_id.id].push(item.subsection_id.id)
         } else {
@@ -113,7 +120,7 @@ export default {
     const q = getters.searchTermQuery
     commit('updateFilteredSections', null)
     if (q !== null) {
-      const response = await api.get(`/api/v2/lifelines_section?num=10000&q=${encodeURIComponent(q)}`)
+      const response = await api.get<any>(`/api/v2/lifelines_section?num=10000&q=${encodeURIComponent(q)}`)
       if (q === getters.searchTermQuery) {
         commit('updateFilteredSections', response.items.map((it: any) => it.id))
       }
@@ -123,14 +130,14 @@ export default {
     const q = getters.searchTermQuery
     commit('updateFilteredSubsections', null)
     if (q !== null) {
-      const response = await api.get(`/api/v2/lifelines_subsection_variable?aggs=x==subsection_agg&q=${encodeURIComponent(q)}`)
+      const response = await api.get<any>(`/api/v2/lifelines_subsection_variable?aggs=x==subsection_agg&q=${encodeURIComponent(q)}`)
       if (q === getters.searchTermQuery) {
         commit('updateFilteredSubsections', response.aggs.xLabels.map((label: string) => parseInt(label, 10)))
       }
     }
   }),
   loadAssessments: tryAction(async ({ commit }: any) => {
-    const response = await api.get('/api/v2/lifelines_assessment')
+    const response = await api.get<any>('/api/v2/lifelines_assessment')
     commit('updateAssessments', response.items.reduce((accum: { [key:number]: Assessment }, assessment: Assessment) => {
       accum[assessment.id] = assessment
       return accum
@@ -138,8 +145,8 @@ export default {
   }),
   loadVariables: tryAction(async ({ state, commit } : any) => {
     const [response0, response1] = await Promise.all([
-      api.get('/api/v2/lifelines_variable?attrs=id,name,label&num=10000&sort=id'),
-      api.get('/api/v2/lifelines_variable?attrs=id,name,label&num=10000&start=10000&sort=id')
+      api.get<any>('/api/v2/lifelines_variable?attrs=id,name,label&num=10000&sort=id'),
+      api.get<any>('/api/v2/lifelines_variable?attrs=id,name,label&num=10000&start=10000&sort=id')
     ])
     const variables: Variable[] = [...response0.items, ...response1.items]
     const variableMap: {[key:number]: Variable} =
@@ -158,9 +165,10 @@ export default {
     if (searchTermQuery !== null) {
       q = `${q};${searchTermQuery}`
     }
-    const response = await api.get(`/api/v2/lifelines_subsection_variable?q=${encodeURIComponent(q)}&attrs=~id,id,subsection_id,variable_id(id,name,label,variants(id,assessment_id))&num=10000&sort=variable_id`)
+    const response = await api.get<any>(`/api/v2/lifelines_subsection_variable?q=${encodeURIComponent(q)}&attrs=~id,id,subsection_id,variable_id(id,name,label,variants(id,assessment_id))&num=10000&sort=variable_id`)
     if ((state.treeSelected === subsectionId) && (searchTermQuery === getters.searchTermQuery)) {
-      commit('updateGridVariables', response.items
+      const jsonData = await response.json()
+      commit('updateGridVariables', jsonData.items
       // map assessment_id to assessmentId somewhere deep in the structure
         .map((sv: any) => ({
           ...sv.variable_id,
@@ -180,9 +188,10 @@ export default {
     if (rsql) {
       url = `${url}&q=${encodeURIComponent(rsql.replace(/ll_nr\./g, ''))}`
     }
-    const response = await api.get(url)
+    const response = await api.get<any>(url)
+    const jsonData = await response.json()
     if (getters.rsql === rsql) {
-      commit('updateParticipantCount', response.total)
+      commit('updateParticipantCount', jsonData.total)
     }
   }),
   loadGridData: tryAction(async ({ commit, getters }: any) => {
@@ -192,7 +201,7 @@ export default {
     if (rsql) {
       url = `${url}&q=${encodeURIComponent(rsql)}`
     }
-    const { aggs: { matrix, xLabels } } = await api.get(url)
+    const { aggs: { matrix, xLabels } } = await api.get<any>(url)
     if (getters.rsql === rsql) {
       const variantCounts = matrix.map((cell: any, index: number) => ({
         variantId: parseInt(xLabels[index].id),
@@ -218,7 +227,7 @@ export default {
       const orderNumber = await createOrder(formData, [ ...formFields, creationDateField ]).catch(() => {
         return Promise.reject(new Error('Failed to create order'))
       })
-      const newOrderResponse = await api.get(`/api/v2/lifelines_order/${orderNumber}`)
+      const newOrderResponse = await api.get<any>(`/api/v2/lifelines_order/${orderNumber}`)
       commit('restoreOrderState', newOrderResponse)
       commit('setToast', { type: 'success', message: 'Saved order with order number ' + orderNumber })
       router.push({ name: 'load', params: { orderNumber: orderNumber } })
@@ -245,14 +254,15 @@ export default {
         return Promise.reject(new Error('Failed to submit order'))
       })
     }
-    const newOrderResponse = await api.get(`/api/v2/lifelines_order/${orderNumber}`)
+    const newOrderResponse = await api.get<any>(`/api/v2/lifelines_order/${orderNumber}`)
     commit('restoreOrderState', newOrderResponse)
     commit('setToast', { type: 'success', message: 'Submitted order with order number ' + orderNumber })
     router.push({ name: 'orders' })
   }),
   load: tryAction(async ({ state, commit }: {state: ApplicationState, commit: any}, orderNumber: string) => {
-    const response = await api.get(`/api/v2/lifelines_order/${orderNumber}`)
-    const cart: Cart = JSON.parse(response.contents)
+    const response = await api.get<any>(`/api/v2/lifelines_order/${orderNumber}`)
+    const jsonData = await response.json()
+    const cart: Cart = JSON.parse(jsonData)
     const { facetFilter, gridSelection } = fromCart(cart, state)
     commit('restoreOrderState', response)
     commit('updateFacetFilter', facetFilter)
