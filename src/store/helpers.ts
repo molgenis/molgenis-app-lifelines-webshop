fafab34d71c2c2078d9abbca94e2615c5b0f8b27
Jeroen van Veen <jvanveen@protonmail.com>
2020-01-23 16:39:45 +0100

    fix: coverage

--- src/store/helpers.ts
+++ src/store/helpers.ts
@@ -24,9 +24,7 @@ export const tryAction = (action: any): any => {
       context.commit('setToast', { message: getErrorMessage(error), type: 'danger', textType: 'light', title: 'Error' })
       // @ts-ignore
       // The exception is also logged to console, but not during tests.
-      if (!global.IS_TEST) {
-        console.trace ? console.trace(error) : console.log(error)
-      }
+      console.trace ? console.trace(error) : console.log(error)
     }
 
     return action(context, payload).catch(errorHandler)
