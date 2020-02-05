f5b3283bc925668cf4ccc60c0321e0e44a7893c7
Max Postema <m.e.postema@rug.nl>
2020-02-05 14:26:16 +0100

    test: improved understandability

--- tests/unit/services/variableSetOrderService.spec.ts
+++ tests/unit/services/variableSetOrderService.spec.ts
@@ -1,40 +1,45 @@
 import { finalVariableSetSort } from '@/services/variableSetOrderService'
 
 describe('finalVariableSetSort', () => {
-  it('sort the variable into its holding sets', () => {
-    const out:any = [{
-      id: 2,
-      subvariables: [ { id: 3 }, { id: 4 }, { id: 5 } ]
-    }, {
-      id: 3,
-      subvariable_of: { id: 2 }
-    }, {
-      id: 5,
-      subvariable_of: { id: 2 }
-    }, {
-      id: 4,
-      subvariable_of: { id: 2 }
-    }, {
-      id: 1
-    }, {
-      id: 6
-    }]
+  // finalVariableSetSort takes a set of data and sorts so that subvariable are always after the parent variable
+  // Note: the original order will be kept
+  it('sorts the sub variables below its parent variable', () => {
+    // data where the variable sets are not correctly together
     const data:any = [{
-      id: 3,
+      id: 1 // Normal variable
+    }, {
+      id: 3, // Sub variable
       subvariable_of: { id: 2 }
     }, {
-      id: 2,
+      id: 2, // Parent variable
       subvariables: [ { id: 3 }, { id: 4 }, { id: 5 } ]
     }, {
-      id: 1
+      id: 6 // Normal variable
     }, {
-      id: 6
-    }, {
-      id: 5,
+      id: 4, // Sub variable
       subvariable_of: { id: 2 }
     }, {
-      id: 4,
+      id: 5, // Sub variable
       subvariable_of: { id: 2 }
+    }]
+
+    // Wished results
+    const out:any = [{
+      id: 1 // Normal variable
+    }, {
+      id: 2, // Parent variable
+      subvariables: [ { id: 3 }, { id: 4 }, { id: 5 } ]
+    }, {
+      id: 3, // Sub variable
+      subvariable_of: { id: 2 }
+    }, {
+      id: 4, // Sub variable
+      subvariable_of: { id: 2 }
+    }, {
+      id: 5, // Normal variable
+      subvariable_of: { id: 2 }
+    }, {
+      id: 6 // Normal variable
     }]
     expect(finalVariableSetSort(data)).toEqual(out)
   })
