dbe1a6281b8ccae32c3750b8a8bbd53eb72c22b7
Max Postema <m.e.postema@rug.nl>
2020-03-25 15:56:46 +0100

    test: fix auto width unit test

--- tests/unit/components/grid/GridComponent.spec.ts
+++ tests/unit/components/grid/GridComponent.spec.ts
@@ -291,8 +291,8 @@ describe('GridComponent.vue', () => {
     let wrapper:any
 
     const propsData = {
-      grid: [[1]],
-      gridAssessments: [{ id: 10 }],
+      gridRows: [[1]],
+      gridColumns: [{ id: 10 }],
       gridVariables: [{
         name: 'a',
         id: 101,
@@ -310,7 +310,7 @@ describe('GridComponent.vue', () => {
         id: 101,
         subvariables: []
       }] })
-      wrapper.setProps({ grid: [[101]] })
+      wrapper.setProps({ gridRows: [[101]] })
       wrapper.vm.$nextTick(() => {
         expect(wrapper).toBeDefined()
         // 0px as jest dom has no width
