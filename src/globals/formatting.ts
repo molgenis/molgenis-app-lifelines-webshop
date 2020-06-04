a0b029157c9b0b9878579028d4f404e32691b053
connoratrug <c.stroomberg@rug.nl>
2020-06-04 15:26:49 +0200

    feat: approx values in grid

--- src/globals/formatting.ts
+++ src/globals/formatting.ts
@@ -1,6 +1,10 @@
 export const formatNumber = function (value: number): string {
-  if (value < 9999) {
+  if (value < 15) {
     return value.toString()
+  } else if (value < 99) {
+    return `≈ ${value - (value % 10)}`
+  } else if (value < 999) {
+    return `≈ ${value - (value % 100)}`
   } else {
     return `${Math.round(value / 1000)}k`
   }
