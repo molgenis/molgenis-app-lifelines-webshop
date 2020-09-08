49451fddc4a8db34d7518569d36985597c67d08e
connoratrug <c.stroomberg@rug.nl>
2020-09-08 15:16:47 +0200

    feat:sort grid variables alphabetically

--- tests/unit/services/variableSetOrderService.spec.ts
+++ tests/unit/services/variableSetOrderService.spec.ts
@@ -7,23 +7,29 @@ describe('finalVariableSetSort', () => {
     // data where the variable sets are not correctly together
     const data:any = [{
       id: 1, // Normal variable
+      name: '1',
       subvariables: []
     }, {
       id: 3, // Sub variable
+      name: '3',
       subvariableOf: { id: 2 },
       subvariables: []
     }, {
       id: 2, // Parent variable
+      name: '2',
       subvariables: [ { id: 3 }, { id: 4 }, { id: 5 } ]
     }, {
       id: 6, // Normal variable
+      name: '6',
       subvariables: []
     }, {
       id: 4, // Sub variable
+      name: '4',
       subvariableOf: { id: 2 },
       subvariables: []
     }, {
       id: 5, // Sub variable
+      name: '5',
       subvariableOf: { id: 2 },
       subvariables: []
     }]
@@ -31,24 +37,30 @@ describe('finalVariableSetSort', () => {
     // Wished results
     const out:any = [{
       id: 1, // Normal variable
+      name: '1',
       subvariables: []
     }, {
       id: 2, // Parent variable
+      name: '2',
       subvariables: [ { id: 3 }, { id: 4 }, { id: 5 } ]
     }, {
       id: 3, // Sub variable
+      name: '3',
       subvariableOf: { id: 2 },
       subvariables: []
     }, {
       id: 4, // Sub variable
+      name: '4',
       subvariableOf: { id: 2 },
       subvariables: []
     }, {
       id: 5, // Normal variable
+      name: '5',
       subvariableOf: { id: 2 },
       subvariables: []
     }, {
       id: 6, // Normal variable
+      name: '6',
       subvariables: []
     }]
     expect(finalVariableSetSort(data)).toEqual(out)
